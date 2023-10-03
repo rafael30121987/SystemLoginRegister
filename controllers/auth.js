745e16b3399019c0860affd1b8b1783b4a9f0ea4
rafael30121987 <114699259+rafael30121987@users.noreply.github.com>
2023-10-02 21:47:57 -0300

    connections&auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,35 @@
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 exports.register = (req, res) => {
     console.log(req.body);
-    res.json("form submitted")
+
+    const {name, email, password, passwordConfirm} = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async(error, results) => {
+        if(error) {
+            console.log(error);
+        }
+        if(results.length > 0) {
+            return res.render('register', {
+                message: 'That email is already in use'
+            });
+        } else if( password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Passwor do not match'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+    }); 
+
 }
